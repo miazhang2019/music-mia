1301b14e1fabb3770e3dbe8dbfedd2275cca0c3d
miazhang2019 <49421385+miazhang2019@users.noreply.github.com>
2022-08-22 00:37:38 +0800

    Add files via upload

---
+++ src/components/recommend/Personalized.vue
@@ -0,0 +1,99 @@
+<template>
+ <div class="personalized">
+    <div class="personalized-top">
+      <h3>{{title}}</h3> 
+      <!-- <h3>推荐歌单</h3> -->
+    </div>
+    <div class="personalized-list">
+      <div class="item" v-for="item in personalized" :key="item.id" @click="selectItem(item.id)">
+        <!-- <img :src="item.picUrl" alt=""> -->
+        <img v-lazy="item.picUrl" alt="">
+        <p>{{ item.name }}</p>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Personalized',
+  props: {
+    personalized: {
+      type: Array,
+      default: () => [],
+      required: true
+    },
+    title: {
+      type: String,
+      default: '',
+      required: true
+    },
+    type: String
+  },
+  methods: {
+    selectItem (id) {
+      console.log(id)
+      this.$emit('select', id, this.type)
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+@import "../../assets/css/variable";
+@import "../../assets/css/mixin";
+.personalized{
+  // 换肤2
+  @include bg_sub_color();
+  overflow: hidden;
+  .personalized-top{
+    width: 100%;
+    height: 84px;
+    // line-height: 84px;
+    @include bg_sub_color();
+    // title和列表的分割
+    border-bottom: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+    padding: 0 20px;
+    overflow: hidden;
+    h3{
+      margin-top: 20px;
+      height: 50px;
+      @include font_size($font_large);
+      font-weight: bold;
+      @include font_color();
+      padding-top: 10px;
+      border-left: 5px solid #000000;
+      padding-left: 20px;
+      @include border_color()
+    }
+  }
+  .personalized-list{
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    padding: 20px 0;
+    .item{
+      width:200px;
+      img{
+        width: 200px;
+        height: 200px;
+        border-radius: 10px;
+      }
+      p{
+        // @include font_size(25Px);
+        @include clamp(2);
+        padding: 20px 0;
+
+        
+        // 换肤1
+        @include font_color();
+        text-align: center;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/recommend/SongList.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="songs">
+    <div class="songs-top">
+    <h3>最新音乐</h3></div>
+    <ul class="songs-list">
+        <li class="item" v-for="(item, index) in songs" :key="index" @click="selectMusic(item.id)">
+            <!-- <img :src="item.song.album.picUrl" alt=""> -->
+            <img v-lazy="item.song.album.picUrl" alt="">
+            <div>
+                <h3>{{item.name}}</h3>
+                <p>{{item.song.artists[0].name}}</p>
+            </div>
+        </li>
+    </ul>
+    </div>
+</template>
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  props: {
+    songs: {
+      type: Array,
+      default: () => []
+    }
+  },
+  name: 'SongList',
+  methods: {
+    ...mapActions([
+      'setFullScreen',
+      // mini和list播放器可在此处补充
+    ]),
+    selectMusic (id) {
+      console.log(id)
+      this.setFullScreen(true)
+      // mini和list播放器可在此处补充
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/css/variable";
+@import "../../assets/css/mixin";
+.songs{
+  @include bg_sub_color();
+  .songs-top{
+    width: 94%;
+    height: 84px;
+    line-height: 84px;
+    @include bg_sub_color();
+    padding: 0 20px;
+    overflow: hidden;
+    h3{
+      margin-top: 20px;
+      height: 50px;
+      @include font_size($font_large);
+      font-weight: bold;
+      @include font_color();
+      padding-top: 10px;
+      border-left: 5px solid #000000;
+      padding-left: 20px;
+      @include border_color()
+    }
+  }
+  .songs-list{
+    width: 100%;
+    overflow: hidden;
+    .item{
+        height: 200px;
+        display: flex;
+        align-items: center;
+        padding: 0 20px;
+        margin-bottom: 20px;
+        border-bottom: 1px solid #ccc;
+        img{
+            width: 150px;
+            height: 150px;
+            border-radius: 20px;
+            // padding-right: 20px;
+        }
+        div{
+            padding-left: 20px;
+            h3{
+                @include font_color()
+            }
+             p {
+                // @include font_size(25px);
+                padding-top: 20px;
+                @include font_color();
+            }
+        }
+    }
+  }
+}
+</style>
