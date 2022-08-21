149050e1825c814ab6bda80e07c89032bf50768e
miazhang2019 <49421385+miazhang2019@users.noreply.github.com>
2022-08-22 00:31:41 +0800

    Add files via upload

---
+++ src/components/detail/DetailTop.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="detail-top">
+        <div class="img-container">
+            <img :src="path" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailTop',
+  props: {
+    path: {
+      type: String,
+      default: '',
+      required: true
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    .detail-top{
+        width: 100%;
+        height: 500px;
+        overflow: hidden;
+        position: relative;
+        img{
+            width: 100%;
+            box-sizing: border-box;
+        }
+    }
+</style>
