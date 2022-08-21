49e47a83277f07510b15c185d5289cc5d1176010
miazhang2019 <49421385+miazhang2019@users.noreply.github.com>
2022-08-22 00:30:35 +0800

    Create DetailBody.vue

---
+++ src/components/detail/DetailBody.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="detail-body">
+    <img :src="path" alt="" />
+    <div class="mask" ref="mask"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Detailbody',
+    components: '',
+    data() {
+        return {
+
+        };
+    },
+    props:{
+      path:{
+        type:String
+      }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped lang="scss">
+.detail-body{
+  width: 100%;
+  height: 500px;
+  overflow: hidden;
+  position: relative;
+  img{
+    width: 100%;
+    box-sizing: border-box;
+  }
+  .mask{
+    position: absolute;
+    background: #000;
+    width: 100%;
+    height: 100%;
+    left: 0;top: 0;
+    opacity: 0;
+  }
+}
+</style>
